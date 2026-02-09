98c91c82020155112cb958647e4c3e720ed498ab
Diogo Nunes <d13nunes@gmail.com>
2026-02-09 15:18:51 +0000

    chore: create project

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { setEngine } from "./app/getEngine";
+import { LoadScreen } from "./app/screens/LoadScreen";
+import { MainScreen } from "./app/screens/main/MainScreen";
+import { userSettings } from "./app/utils/userSettings";
+import { CreationEngine } from "./engine/engine";
+
+/**
+ * Importing these modules will automatically register there plugins with the engine.
+ */
+import "@pixi/sound";
+// import "@esotericsoftware/spine-pixi-v8";
+
+// Create a new creation engine instance
+const engine = new CreationEngine();
+setEngine(engine);
+
+(async () => {
+  // Initialize the creation engine instance
+  await engine.init({
+    background: "#1E1E1E",
+    resizeTo: window,
+    resizeOptions: { minWidth: 300, minHeight: 500, letterbox: false },
+    antialias: true,
+    resolution: window.devicePixelRatio || 2,
+    autoDensity: true,
+  });
+
+  // Initialize the user settings
+  userSettings.init();
+
+  // Show the load screen
+  await engine.navigation.showScreen(LoadScreen);
+  // Show the main screen once the load screen is dismissed
+  await engine.navigation.showScreen(MainScreen);
+})();
